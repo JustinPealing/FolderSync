054b97b7cf45bbcd790afc01aa6b4f749c1c82dc
Justin Pealing <justin@pealing.me.uk>
2017-11-18 21:56:44 +0000

    Initial import

---
+++ FolderSync/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FolderSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                PrintUsage();
+            }
+            new FolderSync(args[2]).CopyFiles(args[0], args[1]);
+        }
+
+        private static void PrintUsage() =>
+            Console.WriteLine("FolderSync <source> <desintation> <record>");
+    }
+}
